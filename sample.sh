d261d20319d6e93b7448c3482a1bcd9d27f6753a
jeremyrhyde <jeremy@viam.com>
2023-05-02 14:24:52 -0400

    update grpc

--- sample.sh
+++ sample.sh
@@ -4,10 +4,10 @@ DATE=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "Run ${DATE}"
 GRPC_SERVER="localhost:8083"
 # GRPC_SERVER=$(sudo lsof -i -n -P | grep carto | awk '{ print $9 }' | head -n 1)
-SLAM_DIR="${HOME}/viam-cartographer"
-API_DIR="${HOME}/api"
+SLAM_DIR="${HOME}/Development/viam-cartographer"
+API_DIR="${HOME}/Development/api"
 cd ${API_DIR}
-PROTOS=<(~/viam-cartographer/grpc/bin/buf build -o -)
+PROTOS=<(~/Development/viam-cartographer/grpc/bin/buf build -o -)
 cd -
 
 mkdir -p ./sample
@@ -16,15 +16,15 @@ mkdir -p ./sample/position
 mkdir -p ./sample/map
 
 grpcurl -max-msg-sz 100000000000000 -d '{"name": "slam"}' \
-  -plaintext -protoset <(~/viam-cartographer/grpc/bin/buf build -o -) $GRPC_SERVER  viam.service.slam.v1.SLAMService/GetInternalState \
-  | jq -r '.internalState' \
-  | base64 -d > ./sample/internal_state/${DATE}.pbstream 
+  -plaintext -protoset <(~/Development/viam-cartographer/grpc/bin/buf build -o -) $GRPC_SERVER  viam.service.slam.v1.SLAMService/GetInternalState \
+  | jq -r '.internalStateChunk' \
+  | ./base64_decode_lines.py > ./sample/internal_state/${DATE}.pbstream 
 
 grpcurl -max-msg-sz 100000000000000 -d '{"name": "slam"}' \
-  -plaintext -protoset <(~/viam-cartographer/grpc/bin/buf build -o -)  $GRPC_SERVER  viam.service.slam.v1.SLAMService/GetPosition > \
+  -plaintext -protoset <(~/Development/viam-cartographer/grpc/bin/buf build -o -)  $GRPC_SERVER  viam.service.slam.v1.SLAMService/GetPosition > \
   ./sample/position/${DATE}.json 
 
 grpcurl -max-msg-sz 100000000000000 -d '{"name": "slam"}' \
-  -plaintext -protoset <(~/viam-cartographer/grpc/bin/buf build -o -) $GRPC_SERVER  viam.service.slam.v1.SLAMService.GetPointCloudMap \
-  | jq -r '.pointCloudPcd' \
-  | base64 -d > ./sample/map/${DATE}.pcd  
+  -plaintext -protoset <(~/Development/viam-cartographer/grpc/bin/buf build -o -) $GRPC_SERVER  viam.service.slam.v1.SLAMService.GetPointCloudMap \
+  | jq -r '.pointCloudPcdChunk' \
+  | ./base64_decode_lines.py > ./sample/map/${DATE}.pcd  
